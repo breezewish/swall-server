ade866029f3f8163902218e5d10ad68be86ea47a
ShiehShieh <xie510894496@gmail.com>
2014-10-28 22:50:45 +0800

    Bug Fixed.

--- build/routes/index.js
+++ build/routes/index.js
@@ -15,6 +15,7 @@
   });
 
   router.post('/:id/keywords', function(req, res) {
+    console.log(req.body.keywords, typeof req.body.keywords);
     if (req.body.keywords && typeof req.body.keywords === 'array') {
       info.keywords['id_' + req.params.id] = req.body.keywords;
       filters['id_' + req.params.id] = require('keyword-filter').init(req.body.keywords);
